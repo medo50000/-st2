83a0739af4deaad0f0a3cb41e1af524a8bea7e1b
medo50000 <45098450+medo50000@users.noreply.github.com>
2019-04-12 14:19:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 client.on('ready', function(){
  console.log(`Logged in as ${client.user.tag}!`);
     
-   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
+   // var s = ['https://cdn.discordapp.com/attachments/521316890216366084/566235728141352960/JPEG_20190412_015318.jpg','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
    var s = ['483055660209012736','480169573530861578','483055655800930315'];
     setInterval(function (){
     client.user.setPresence({
